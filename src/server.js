e213e708f61f16b2977ee845f330213b3b921662
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-08-06 00:38:52 -0300

    feat: Add authentication middleware for verifying JWT tokens and handling access control

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 
 import publicRoutes from "./routes/public.js"
 import privateRoutes from "./routes/private.js"
+import auth from "../middlewares/auth.js"
 
 const app = express()
 app.use(express.json())
@@ -14,7 +15,7 @@ app.use('/', publicRoutes)
 
 
 // Rota Privada 3- Listar Usuarios - Rota Privada 
-app.use('/', privateRoutes)
+app.use('/', auth,  privateRoutes)
 
 app.listen(PORT, () => {
     console.log(`O servidor esta rodando na PORTA: ${PORT}`)
