e6349fcf188d6829b18babec5447d19baced297c
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-08-05 23:32:21 -0300

    feat:Add login route with email verification, password comparison, and JWT token generation

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,10 +1,14 @@
 import express from "express"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 import { PrismaClient } from "@prisma/client"
 
 
 const prisma = new PrismaClient()
 const router = express.Router()
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 
 // Rota Cadastro
@@ -28,10 +32,46 @@ router.post('/cadastro', async (req, res) => {
     return res.status(201).json(usersDB)
 
     } catch (error) {
-       res.status(500).json({ message: "Erro no serbidor, tente novamente..."})
+       return res.status(409).json({ message: "E-mail já existente, tente novamente com outro email."})
     }
-  
 })
+
+
 // Rota Login
+router.post('/login', async (req, res) => {
+    try {
+        const userInfo = req.body
+
+        // Busca o usuario no banco de dados
+        const user = await prisma.user.findUnique({
+             where: 
+             {email : userInfo.email}
+            })
+
+        // Verifica se o usuario existe no banco de dados
+        if(!user) {
+            return res.status(404).json({message: "E-mail não encontrado."})
+        }
+
+        const isMatch = await bcrypt.compare(userInfo.password, user.password)
+
+        // Compara a senha do banco com a que o usuario digitou.
+        if(!isMatch){
+            return res.status(400).json({ message: "Senha Incorreta, tente novamente."})
+        }
+
+        // Gerar o token jwt (jsonwebtoken)
+        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '2m'})
+
+        return res.status(200).json(token)
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Error no servidor, tente novamente!"})
+    }
+
+
+})
 
 export default router;
